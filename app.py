3e75f92f3ea0191a298255962d74b089ac0d2a1c
ThomG1 <thomas.gwyther@hotmail.co.uk>
2023-03-02 14:47:55 +0000

    Add registration form

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,11 @@ mongo = PyMongo(app)
 def get_records():
     records = mongo.db.records.find()
     return render_template("records.html", records=records)
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("sign-up.html")
 
 
 if __name__ == "__main__":
