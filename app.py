e745ca089a0c1dae8139ef60acbeaf6882d908c2
ThomG1 <thomas.gwyther@hotmail.co.uk>
2023-03-03 14:59:31 +0000

    Add sign-up functionality

--- app.py
+++ app.py
@@ -27,6 +27,24 @@ def get_records():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("signup"))
+
+        signup = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(signup)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Sign-Up Complete!")
     return render_template("sign-up.html")
 
 
